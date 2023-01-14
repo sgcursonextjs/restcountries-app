62c01ebaa791fd41e9545275ed575305bfb55b5a
sgcursonextjs <sgcursonextjs@gmail.com>
2023-01-13 23:42:21 -0400

    Agregando dark mode

--- components/header.js
+++ components/header.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 import styles from '../styles/Header.module.css'
-export const Header = () =>{
+export const Header = (props) =>{
 
-  
+  const { dark } = props;
     return(
         <header id={styles.sombra} className="d-flex justify-content-between p-3">
-            <h4 className="ms-5">Where in the World?</h4>
-            <p  className="me-5" style={{cursor: 'pointer'}}>
+            <h4 style={{color: dark ? "white" : "black"}}  className="ms-5">Where in the World?</h4>
+            <p style={{color: dark ? "white" : "black", cursor: 'pointer'}} onClick={props.darkMode}  className="me-5">
                 <i class="bi bi-moon"></i>  
                 <span className='ms-2'>Dark Mode</span>
             </p>
